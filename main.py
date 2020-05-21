9746156f0b90949b48de5d8234e08ac14c296c52
nguyenhongphuc2019 <nguyen.hong.phuc.20191997@gmail.com>
2020-05-21 22:39:16 +0700

    perceptron

---
+++ main.py
@@ -0,0 +1,58 @@
+# 1. PLA có thể cho vô số nghiệm khác nhau
+
+import numpy as np
+
+np.random.seed(2)
+means = [[2, 2], [4, 2]]
+cov = [[.3, .2], [.2, .3]]
+N = 10
+X0 = np.random.multivariate_normal(means[0], cov, N).T
+X1 = np.random.multivariate_normal(means[1], cov, N).T
+
+X = np.concatenate((X0, X1), axis=1)
+y = np.concatenate((np.ones((1, N)), -1 * np.ones((1, N))), axis=1)
+X = np.concatenate((np.ones((1, 2 * N)), X), axis=0)
+
+
+def h(w, x):
+    return np.sign(np.dot(w.T, x))
+
+
+def has_converged(X, y, w):
+    return np.array_equal(h(w, X), y)  # True if h(w, X) == y else False
+
+
+def perceptron(X, y, w_init):
+    w = [w_init]
+    N = X.shape[1]
+    mis_points = []
+    pocket = []
+    for idx in range(5):
+        # mix data
+        mix_id = np.random.permutation(N)
+        for i in range(N):
+            xi = X[:, mix_id[i]].reshape(3, 1)
+            yi = y[0, mix_id[i]]
+
+            # check if x[i] is miss_classified
+            if h(w[-1], xi)[0] != yi:
+                mis_points.append(mix_id[i])
+                w_new = w[-1] + yi * xi
+                w.append(w_new)
+
+            if len(pocket) == 0:
+                pocket.append([w_new, len(mis_points)])
+            elif len(mis_points) < pocket[-1][-1]:
+                    pocket.remove(pocket[-1])
+                    pocket.append([w_new, len(mis_points)])
+
+        # if has_converged(X, y, w[-1]):
+        #     break
+    return (w, pocket)
+
+
+d = X.shape[0]
+w_init = np.random.randn(d, 1)
+w, mis_points = perceptron(X, y, w_init)
+# print(mis_points)
+print(w)
